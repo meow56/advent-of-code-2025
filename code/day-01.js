cdcb8c0e8c16a653f36770c0abb4dde66c4449e9
meow56 <spinattacktotheface@gmail.com>
2025-11-30 23:11:13 -0600

    Day 1-1
    Me and the boys forget how to use the local webserver. Oops.

--- code/day-01.js
+++ code/day-01.js
@@ -1,9 +1,24 @@
 "use strict";
 
 function day01(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /(L|R)(\d+)/g;
 	let entry;
+	let moves = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		moves.push(entry[1] === "L" ? -entry[2] : +entry[2]);
 	}
+
+	function mod(x, base) {
+		return ((x % base) + base) % base;
+	}
+
+	let pos = 50;
+	let numZeros = 0;
+	for(let move of moves) {
+		pos = mod(pos + move, 100);
+		if(pos === 0) {
+			numZeros++;
+		}
+	}
+	displayCaption(`The number of zeros is ${numZeros}.`);
 }
