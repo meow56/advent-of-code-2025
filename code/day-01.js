a13e2bcd14d1a41ff399ebcd0d639d63117b8032
meow56 <spinattacktotheface@gmail.com>
2025-11-30 23:57:16 -0600

    Day 1-3
    Display done. Also forgot to remove stuff from the html file, so fixed that. No more 404 spam in the console.

--- code/day-01.js
+++ code/day-01.js
@@ -12,30 +12,49 @@ function day01(input) {
 		return ((x % base) + base) % base;
 	}
 
+	assignPane('part1', 'Part 1');
+	assignPane('part2', 'Part 2');
+	displayToPane('part1', 'START     -> 50');
+	displayToPane('part2', 'START     -> 50');
+
 	let pos = 50;
 	let otherPos = 50;
 	let numZeros = 0;
 	let secondZeros = 0;
 	for(let move of moves) {
 		pos = mod(pos + move, 100);
+		let p1Text = (move < 0 ? `LEFT ` : `RIGHT`) + ` ${((Math.abs(move)).toString()).padStart(3, ' ')} -> ${pos.toString().padStart(2, '0')}`;
 		if(pos === 0) {
 			numZeros++;
+			p1Text += ` [${numZeros}]`;
 		}
+		displayToPane('part1', p1Text);
 
 		let newPos = otherPos + move;
-		if(otherPos === 0 && move < 0) secondZeros--;
+		let totalZeroClicks = 0;
+		if(otherPos === 0 && move < 0) totalZeroClicks--;
 		while(newPos < 0) {
 			newPos += 100;
-			secondZeros++;
+			totalZeroClicks++;
 		}
-		if(newPos === 0) secondZeros++;
+		if(newPos === 0) totalZeroClicks++;
 		while(newPos > 99) {
 			newPos -= 100;
-			secondZeros++;
+			totalZeroClicks++;
 		}
+		secondZeros += totalZeroClicks;
 		otherPos = newPos;
+		let p2Text = (move < 0 ? `LEFT ` : `RIGHT`) + ` ${((Math.abs(move)).toString()).padStart(3, ' ')} -> ${otherPos.toString().padStart(2, '0')}`;
+		if(totalZeroClicks !== 0) {
+			p2Text += ` (${totalZeroClicks < 10 ? " " : ""}+${totalZeroClicks}) [${secondZeros}]`;
+		}
+		displayToPane('part2', p2Text);
 	}
 	if(otherPos === 0) secondZeros++;
 	displayCaption(`The number of zeros is ${numZeros}.`);
-	displayCaption(`The number of 0x434C49434B zeros is ${secondZeros}.`);
+	displayCaption(`The number of zeros using password method CLICK is ${secondZeros}.`);
+	displayCaption(`The commands are displayed.`);
+	displayCaption(`Click the buttons to switch between parts 1 and 2.`);
+	displayCaption(`Numbers in [] indicate the total zeros so far.`);
+	displayCaption(`Numbers in () indicate how many zeros were added in this command.`);
 }
