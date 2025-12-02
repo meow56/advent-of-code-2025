b5c096fc22f635aff5347ce529624b3094c669e9
meow56 <spinattacktotheface@gmail.com>
2025-12-01 23:11:37 -0600

    Day 2-2
    Who needs regex or anything like that? For loops all the way!

--- code/day-02.js
+++ code/day-02.js
@@ -9,17 +9,37 @@ function day02(input) {
 	}
 
 	let sum = 0;
+	let sum2 = 0;
 	for(let range of ranges) {
 		for(let i = range[0]; i <= range[1]; i++) {
 			let len = i.toString().length;
-			if(len % 2 !== 0) continue;
-			let firstHalf = i.toString().slice(0, len / 2);
-			let secondHalf = i.toString().slice(len / 2);
-			if(firstHalf === secondHalf) {
-				console.log(i);
-				sum += i;
+			if(len % 2 === 0) {
+				let firstHalf = i.toString().slice(0, len / 2);
+				let secondHalf = i.toString().slice(len / 2);
+				if(firstHalf === secondHalf) {
+					sum += i;
+				}
+			}
+
+			let strI = i.toString();
+			for(let cutSize = 1; cutSize < len; cutSize++) {
+				if(len % cutSize !== 0) continue;
+				let base = strI.slice(0, cutSize);
+				let sillyID = true;
+				for(let j = cutSize; j < len; j += cutSize) {
+					let testCut = strI.slice(j, j + cutSize);
+					if(testCut !== base) {
+						sillyID = false;
+						break;
+					}
+				}
+				if(sillyID) {
+					sum2 += i;
+					break;
+				}
 			}
 		}
 	}
 	displayCaption(`The sum is ${sum}.`);
+	displayCaption(`The sum2 is ${sum2}.`);
 }
