dc7e1927cc3a12c7fbb953872463c59d5f61b653
meow56 <spinattacktotheface@gmail.com>
2025-12-02 23:09:56 -0600

    Day 3-1
    Not too bad...

--- code/day-03.js
+++ code/day-03.js
@@ -1,9 +1,32 @@
 "use strict";
 
 function day03(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /(\d+)/g;
 	let entry;
+	let lines = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		lines.push(entry[1]);
 	}
+
+	let totalJoltage = 0;
+	for(let line of lines) {
+		let maxJoltageFound = false;
+		for(let i = 9; i >= 0; i--) {
+			let firstIndex = line.indexOf(i);
+			if(firstIndex !== -1 && firstIndex !== line.length - 1) {
+				for(let j = 9; j >= 0; j--) {
+					let secondIndex = line.indexOf(j, firstIndex + 1);
+					if(secondIndex !== -1) {
+						console.log(`${line}: ${i} ${j}`);
+						totalJoltage += (10 * i) + j;
+						maxJoltageFound = true;
+						break;
+					}
+				}
+			}
+			if(maxJoltageFound) break;
+		}
+	}
+
+	displayCaption(`The total joltage you can get is ${totalJoltage}.`);
 }
