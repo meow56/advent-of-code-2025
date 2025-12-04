55294883683df8ba0d6c654c2f9aef284b24983a
meow56 <spinattacktotheface@gmail.com>
2025-12-03 23:12:31 -0600

    Day 4-2
    I'm sure this is very efficient, TRUST

--- code/day-04.js
+++ code/day-04.js
@@ -35,5 +35,33 @@ function day04(input) {
 		}
 	}
 
+	let removedPaper = 1;
+	let totalRemoved = 0;
+	while(removedPaper !== 0) {
+		removedPaper = 0;
+		let accessiblePapers = [];
+		for(let y = 0; y < grid.length; y++) {
+			for(let x = 0; x < grid[y].length; x++) {
+				if(grid[y][x] !== "@") continue;
+				let adjacentPaper = 0;
+				for(let neighbor of NEIGHBORS) {
+					if(grid[y + neighbor[1]]?.[x + neighbor[0]] === "@") {
+						adjacentPaper++;
+					}
+				}
+				if(adjacentPaper < 4) {
+					accessiblePapers.push([x, y]);
+				}
+			}
+		}
+
+		for(let coord of accessiblePapers) {
+			grid[coord[1]][coord[0]] = ".";
+		}
+		removedPaper = accessiblePapers.length;
+		totalRemoved += removedPaper;
+	}
+
 	displayCaption(`The number of accessible rolls is ${accessiblePaper}.`);
+	displayCaption(`The total number of rolls you can remove is ${totalRemoved}.`);
 }
