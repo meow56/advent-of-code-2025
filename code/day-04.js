64f78d071cfc839e0abf026cff8210bce51f0c99
meow56 <spinattacktotheface@gmail.com>
2025-12-03 23:07:43 -0600

    Day 4-1
    On today's episode of silly bugs: forgetting to check if the current tile is a paper roll.

--- code/day-04.js
+++ code/day-04.js
@@ -1,9 +1,39 @@
 "use strict";
 
 function day04(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /.+/g;
 	let entry;
+	let grid = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		grid.push(entry[0].split(""));
 	}
+
+	const NEIGHBORS = [
+		[-1, -1],
+		[-1, 0],
+		[-1, 1],
+		[0, -1],
+		[0, 1],
+		[1, -1],
+		[1, 0],
+		[1, 1],
+	];
+
+	let accessiblePaper = 0;
+	for(let y = 0; y < grid.length; y++) {
+		for(let x = 0; x < grid[y].length; x++) {
+			if(grid[y][x] !== "@") continue;
+			let adjacentPaper = 0;
+			for(let neighbor of NEIGHBORS) {
+				if(grid[y + neighbor[1]]?.[x + neighbor[0]] === "@") {
+					adjacentPaper++;
+				}
+			}
+			if(adjacentPaper < 4) {
+				accessiblePaper++;
+			}
+		}
+	}
+
+	displayCaption(`The number of accessible rolls is ${accessiblePaper}.`);
 }
