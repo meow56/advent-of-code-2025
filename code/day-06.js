de16b24d0d6da70050cb4f4ee8723b3f5b18098e
meow56 <spinattacktotheface@gmail.com>
2025-12-05 23:20:31 -0600

    Day 6-2
    On today's episode of strange code, we have row.join("").trim().length === 0.

--- code/day-06.js
+++ code/day-06.js
@@ -4,18 +4,27 @@ function day06(input) {
 	const FILE_REGEX = /^.+$/gm;
 	let entry;
 	let numGrid = [];
+	let wrongGrid = [];
 	while(entry = FILE_REGEX.exec(input)) {
 		numGrid.push(entry[0].split(" ").filter(e => e !== ""));
+		wrongGrid.push(entry[0].split(""));
 	}
 
-	let transposedGrid = [];
-	for(let i = 0; i < numGrid[0].length; i++) {
-		let newRow = [];
-		for(let j = 0; j < numGrid.length; j++) {
-			newRow.push(numGrid[j][i]);
+	function transpose(grid) {
+		let transposedGrid = [];
+		for(let i = 0; i < grid[0].length; i++) {
+			let newRow = [];
+			for(let j = 0; j < grid.length; j++) {
+				newRow.push(grid[j][i]);
+			}
+			transposedGrid.push(newRow);
 		}
-		transposedGrid.push(newRow);
+		return transposedGrid;
 	}
+
+	let transposedGrid = transpose(numGrid);
+	let transposedTextGrid = transpose(wrongGrid).toReversed();
+	console.log(transposedTextGrid);
 
 	let sum = 0;
 	for(let row of transposedGrid) {
@@ -31,5 +40,18 @@ function day06(input) {
 		sum += answer;
 	}
 
+	let trueSum = 0;
+	let stack = [];
+	for(let row of transposedTextGrid) {
+		if(row.join("").trim().length === 0) continue;
+		let operation = row.pop();
+		stack.push(+(row.join("")));
+		if(operation !== " ") {
+			trueSum += stack.reduce((acc, val) => (operation === "*" ? acc * val : acc + val));
+			stack = [];
+		}
+	}
+
 	displayCaption(`The grand total is ${sum}.`);
+	displayCaption(`The actual grand total is ${trueSum}.`);
 }
