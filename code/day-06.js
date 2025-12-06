74f2729392700681ab007f3224141cd45bb79b6b
meow56 <spinattacktotheface@gmail.com>
2025-12-05 23:07:53 -0600

    Day 6-1
    Too bad I don't have a transpose function...

--- code/day-06.js
+++ code/day-06.js
@@ -1,9 +1,35 @@
 "use strict";
 
 function day06(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /^.+$/gm;
 	let entry;
+	let numGrid = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		numGrid.push(entry[0].split(" ").filter(e => e !== ""));
 	}
+
+	let transposedGrid = [];
+	for(let i = 0; i < numGrid[0].length; i++) {
+		let newRow = [];
+		for(let j = 0; j < numGrid.length; j++) {
+			newRow.push(numGrid[j][i]);
+		}
+		transposedGrid.push(newRow);
+	}
+
+	let sum = 0;
+	for(let row of transposedGrid) {
+		let operation = row.pop();
+		let answer = +(operation === "*");
+		for(let num of row) {
+			if(operation === "+") {
+				answer += +num;
+			} else if(operation === "*") {
+				answer *= +num;
+			}
+		}
+		sum += answer;
+	}
+
+	displayCaption(`The grand total is ${sum}.`);
 }
