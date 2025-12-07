8e959364038049f3095cbf6e865abe6b76644256
meow56 <spinattacktotheface@gmail.com>
2025-12-06 23:08:36 -0600

    Day 7-1
    I forgot to read the part where we're supposed to count the number of splits instead of the number of beams leaving the manifold.

--- code/day-07.js
+++ code/day-07.js
@@ -1,9 +1,41 @@
 "use strict";
 
 function day07(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /.+/g;
 	let entry;
+	let grid = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		grid.push(entry[0].split(""));
 	}
+
+	let startPos;
+	for(let y = 0; y < grid.length; y++) {
+		for(let x = 0; x < grid[y].length; x++) {
+			if(grid[y][x] === "S") {
+				startPos = [x, y];
+				break;
+			}
+		}
+		if(startPos) break;
+	}
+
+	let splits = 0;
+	let beamsX = [startPos[0]];
+	for(let y = 0; y < grid.length; y++) {
+		let newBeamsX = [];
+		for(let beam of beamsX) {
+			if(grid[y][beam] === "^") {
+				splits++;
+				if(!newBeamsX.includes(beam - 1)) 
+					newBeamsX.push(beam - 1);
+				if(!newBeamsX.includes(beam + 1)) 
+					newBeamsX.push(beam + 1);
+			} else {
+				if(!newBeamsX.includes(beam)) 
+					newBeamsX.push(beam);
+			}
+		}
+		beamsX = newBeamsX;
+	}
+	displayCaption(`The number of times the beam splits is ${splits}.`);
 }
