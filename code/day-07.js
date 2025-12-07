5de3807eb16b897707b0ec63498382e3cfd51bc7
meow56 <spinattacktotheface@gmail.com>
2025-12-06 23:14:58 -0600

    Day 7-2
    Well, that number was a little more than I expected...

--- code/day-07.js
+++ code/day-07.js
@@ -20,22 +20,26 @@ function day07(input) {
 	}
 
 	let splits = 0;
-	let beamsX = [startPos[0]];
+	let beamsX = new Map([[startPos[0], 1]]);
 	for(let y = 0; y < grid.length; y++) {
-		let newBeamsX = [];
-		for(let beam of beamsX) {
+		let newBeamsX = new Map();
+		for(let [beam, multiplicity] of beamsX.entries()) {
 			if(grid[y][beam] === "^") {
 				splits++;
-				if(!newBeamsX.includes(beam - 1)) 
-					newBeamsX.push(beam - 1);
-				if(!newBeamsX.includes(beam + 1)) 
-					newBeamsX.push(beam + 1);
+				newBeamsX.set(beam - 1, multiplicity + (newBeamsX.get(beam - 1) ?? 0));
+				newBeamsX.set(beam + 1, multiplicity + (newBeamsX.get(beam + 1) ?? 0));
 			} else {
-				if(!newBeamsX.includes(beam)) 
-					newBeamsX.push(beam);
+				newBeamsX.set(beam, multiplicity + (newBeamsX.get(beam) ?? 0));
 			}
 		}
 		beamsX = newBeamsX;
 	}
+
+	let timelines = 0;
+	for(let mult of beamsX.values()) {
+		timelines += mult;
+	}
+
 	displayCaption(`The number of times the beam splits is ${splits}.`);
+	displayCaption(`The number of timelines is ${timelines}.`);
 }
