a1cadbb9c5d7195843c17b88b97ac24a35dbcb26
meow56 <spinattacktotheface@gmail.com>
2025-12-11 22:32:28 -0600

    Day 11-1
    Wasn't that a lot simpler than simplex?

--- code/day-11.js
+++ code/day-11.js
@@ -1,9 +1,40 @@
 "use strict";
 
 function day11(input) {
-	const FILE_REGEX = /./g;
+	const FILE_REGEX = /([a-z]{3}): ((?:[a-z]{3} ?)+)/g;
 	let entry;
+	let devices = [];
 	while(entry = FILE_REGEX.exec(input)) {
-
+		devices.push(new Device(entry[1], entry[2].split(" ")));
 	}
+
+	function Device(name, outs) {
+		this.name = name;
+		this.outs = outs;
+	}
+
+	devices.push(new Device("out", []));
+
+	Device.prototype.connect = function() {
+		this.outs = this.outs.map(e => devices.find(d => d.name === e));
+	}
+
+	devices.forEach(d => d.connect());
+
+	let you = devices.find(d => d.name === "you");
+	const numPaths = new Map();
+	function pathsToOut(currPos) {
+		if(currPos.name === "out") return 1;
+		if(numPaths.has(currPos.name)) return numPaths.get(currPos.name);
+		let sum = 0;
+		for(let neighbor of currPos.outs) {
+			sum += pathsToOut(neighbor);
+		}
+
+		numPaths.set(currPos.name, sum);
+		return sum;
+	}
+
+	let result = pathsToOut(you);
+	displayCaption(`The number of paths to out is ${result}.`);
 }
